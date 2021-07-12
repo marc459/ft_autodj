b73e0365c13013a958a83d694135fea1baad2d3c
marc459 <santosserranomarcos@gmail.com>
2021-07-12 23:50:55 +0200

    using aplay

--- includes/ft_autodj.h
+++ includes/ft_autodj.h
@@ -6,18 +6,25 @@
 /*   By: msantos- <msantos-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/17 19:36:46 by msantos-          #+#    #+#             */
-/*   Updated: 2021/07/12 21:08:00 by msantos-         ###   ########.fr       */
+/*   Updated: 2021/07/12 21:19:34 by msantos-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # ifndef FT_AUTODJ_H
 # define FT_AUTODJ_H
 
-#include <signal.h>
 #include <stdio.h>
 #include <unistd.h>
 #include "../ft_printf_42/src/printf.h"
 #include "../libft_42/libft.h"
+
+# if defined(__APPLE__)
+#  define PLAY "afplay"
+#  define SO "MACOS"
+# else
+#  define PLAY "aplay"
+#  define SO "LINUX"
+# endif
 
 void		printerr(char *err);
 
--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: msantos- <msantos-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/17 16:49:15 by msantos-          #+#    #+#             */
-/*   Updated: 2021/07/12 21:09:33 by msantos-         ###   ########.fr       */
+/*   Updated: 2021/07/12 21:35:18 by msantos-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,11 @@
 int		main(int argc, char **argv)
 {
 	ft_printf("AUTODJ");
-		
-
+	system(ft_strjoin(PLAY, " music/DaftPunk_OneMoreTime.mp3 &"));
+	while(1)
+	{
+		sleep(1);
+	}
+	system("pkill afplay");
 	return (0);
 }
